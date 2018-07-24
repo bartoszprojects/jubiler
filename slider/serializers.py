7e1f643d9b3878d43fb802918f5eb2a89423123b
ThomasLech <itsawesome17@gmail.com>
2018-07-24 17:28:19 +0200

    Added a new field to ContactSerializer.

--- slider/serializers.py
+++ slider/serializers.py
@@ -48,16 +48,15 @@ class ServiceImageSerializer(serializers.ModelSerializer):
         fields = ('id','service_images', 'title', 'image')
 
 class ContactSerializer(serializers.Serializer):
+    image = serializers.ImageField(required=False, max_length=None, allow_empty_file=True, use_url=True)
     name = serializers.CharField(max_length=200, required=False)
     email = serializers.CharField(max_length=200, required=True)
     phone_number = serializers.CharField(max_length=200, required=False)
     message = serializers.CharField(max_length=200, required=True)
+
     def save(self):
+        self.image = self.validated_data.get('image')
         self.name = self.validated_data['name']
         self.email = self.validated_data['email']
-        self.phone_number = self.validated_data['phone_number']
+        self.phone_number = self.validated_data.get('phone_number')
         self.message = self.validated_data['message']
-
-
-
-
